69f85f2aaa417f4917f3c1361eba728f032d9526
zzL1ng <haruhizz@outlook.com>
2023-05-14 17:16:11 +0800

    init lab1_c2

--- kernel/elf.h
+++ kernel/elf.h
@@ -5,7 +5,42 @@
 #include "process.h"
 
 #define MAX_CMDLINE_ARGS 64
-
+#define SHT_NULL            0        /* Section header table entry unused */
+#define SHT_PROGBITS        1        /* Program data */
+#define SHT_SYMTAB          2        /* Symbol table */
+#define SHT_STRTAB          3        /* String table */
+#define SHT_RELA            4        /* Relocation entries with addends */
+#define SHT_HASH            5        /* Symbol hash table */
+#define SHT_DYNAMIC         6        /* Dynamic linking information */
+#define SHT_NOTE            7        /* Notes */
+#define SHT_NOBITS          8        /* Program space with no data (bss) */
+#define SHT_REL             9        /* Relocation entries, no addends */
+#define SHT_SHLIB          10        /* Reserved */
+#define SHT_DYNSYM         11        /* Dynamic linker symbol table */
+#define SHT_INIT_ARRAY     14        /* Array of constructors */
+#define SHT_FINI_ARRAY     15        /* Array of destructors */
+#define SHT_PREINIT_ARRAY  16        /* Array of pre-constructors */
+#define SHT_GROUP          17        /* Section group */
+#define SHT_SYMTAB_SHNDX   18        /* Extended section indeces */
+#define    SHT_NUM         19        /* Number of defined types.  */
+#define SHT_LOOS           0x60000000    /* Start OS-specific.  */
+#define SHT_GNU_ATTRIBUTES 0x6ffffff5    /* Object attributes.  */
+#define SHT_GNU_HASH       0x6ffffff6    /* GNU-style hash table.  */
+#define SHT_GNU_LIBLIST    0x6ffffff7    /* Prelink library list */
+#define SHT_CHECKSUM       0x6ffffff8    /* Checksum for DSO content.  */
+#define SHT_LOSUNW         0x6ffffffa    /* Sun-specific low bound.  */
+#define SHT_SUNW_move      0x6ffffffa
+#define SHT_SUNW_COMDAT    0x6ffffffb
+#define SHT_SUNW_syminfo   0x6ffffffc
+#define SHT_GNU_verdef     0x6ffffffd    /* Version definition section.  */
+#define SHT_GNU_verneed    0x6ffffffe    /* Version needs section.  */
+#define SHT_GNU_versym     0x6fffffff    /* Version symbol table.  */
+#define SHT_HISUNW         0x6fffffff    /* Sun-specific high bound.  */
+#define SHT_HIOS           0x6fffffff    /* End OS-specific type */
+#define SHT_LOPROC         0x70000000    /* Start of processor-specific */
+#define SHT_HIPROC         0x7fffffff    /* End of processor-specific */
+#define SHT_LOUSER         0x80000000    /* Start of application-specific */
+#define SHT_HIUSER         0x8fffffff    /* End of application-specific */
 // elf header structure
 typedef struct elf_header_t {
   uint32 magic;
@@ -37,6 +72,33 @@ typedef struct elf_prog_header_t {
   uint64 align;  /* Segment alignment */
 } elf_prog_header;
 
+// elf section header
+typedef struct elf_sect_header_t{
+    uint32 name;
+    uint32 type;
+    uint64 flags;
+    uint64 addr;
+    uint64 offset;
+    uint64 size;
+    uint32 link;
+    uint32 info;
+    uint64 addralign;
+    uint64 entsize;
+} elf_sect_header;
+
+// compilation units header (in debug line section)
+typedef struct __attribute__((packed)) {
+    uint32 length;
+    uint16 version;
+    uint32 header_length;
+    uint8 min_instruction_length;
+    uint8 default_is_stmt;
+    int8 line_base;
+    uint8 line_range;
+    uint8 opcode_base;
+    uint8 std_opcode_lengths[12];
+} debug_header;
+
 #define ELF_MAGIC 0x464C457FU  // "\x7FELF" in little endian
 #define ELF_PROG_LOAD 1
 
@@ -59,5 +121,6 @@ elf_status elf_init(elf_ctx *ctx, void *info);
 elf_status elf_load(elf_ctx *ctx);
 
 void load_bincode_from_host_elf(process *p);
+void load_debug_line(elf_ctx *ctx);
 
 #endif
--- kernel/machine/mtrap.c
+++ kernel/machine/mtrap.c
@@ -2,17 +2,61 @@
 #include "kernel/process.h"
 #include "spike_interface/spike_utils.h"
 
-static void handle_instruction_access_fault() { panic("Instruction access fault!"); }
+#include "spike_interface/spike_file.h"
+#include <string.h>
 
-static void handle_load_access_fault() { panic("Load access fault!"); }
+char full_path[256];
+char full_file[5120];
+struct stat file_stat;
+void error_displayer() {
+  uint64 exp_addr = read_csr(mepc);
+  for(int i=0; i<current->line_ind; i++) 
+  {
+    if(exp_addr < current->line[i].addr)
+    {
+      addr_line *exp = current->line + i - 1;
+      int dirlen = strlen(current->dir[current->file[exp->file].dir]);   
+      strcpy(full_path, current->dir[current->file[exp->file].dir]);
+      full_path[dirlen] = '/';
+      strcpy(full_path+dirlen+1, current->file[exp->file].file);   
+      spike_file_t * _file_ = spike_file_open(full_path, O_RDONLY, 0);
+      spike_file_stat(_file_, &file_stat);
+      spike_file_read(_file_, full_file, file_stat.st_size);
+      spike_file_close(_file_);
+      int offset = 0, count = 0;
+      while (offset < file_stat.st_size) 
+      {
+        int tmp = offset;
+        while (tmp < file_stat.st_size && full_file[tmp] != '\n') tmp++;
+        if (count == exp->line - 1) 
+        {
+          full_file[tmp] = '\0';
+          sprint("Runtime error at %s:%d\n%s\n", full_path, exp->line, full_file + offset);
+          break;
+        } 
+        else
+        {
+          count++;
+          offset = tmp + 1;
+        }
+      }
+      break;
+    }
+  }
+}
 
-static void handle_store_access_fault() { panic("Store/AMO access fault!"); }
+static void handle_instruction_access_fault() {error_displayer(); panic("Instruction access fault!"); }
 
-static void handle_illegal_instruction() { panic("Illegal instruction!"); }
+static void handle_load_access_fault() {error_displayer(); panic("Load access fault!"); }
 
-static void handle_misaligned_load() { panic("Misaligned Load!"); }
+static void handle_store_access_fault() {error_displayer(); panic("Store/AMO access fault!"); }
 
-static void handle_misaligned_store() { panic("Misaligned AMO!"); }
+static void handle_illegal_instruction() {error_displayer(); panic("Illegal instruction!"); }
+
+static void handle_misaligned_load() {error_displayer(); panic("Misaligned Load!"); }
+
+static void handle_misaligned_store() {error_displayer(); panic("Misaligned AMO!"); }
+
 
 // added @lab1_3
 static void handle_timer() {
@@ -44,8 +88,8 @@ void handle_mtrap() {
     case CAUSE_ILLEGAL_INSTRUCTION:
       // TODO (lab1_2): call handle_illegal_instruction to implement illegal instruction
       // interception, and finish lab1_2.
-      panic( "call handle_illegal_instruction to accomplish illegal instruction interception for lab1_2.\n" );
-
+      //panic( "call handle_illegal_instruction to accomplish illegal instruction interception for lab1_2.\n" );
+      handle_illegal_instruction();
       break;
     case CAUSE_MISALIGNED_LOAD:
       handle_misaligned_load();
