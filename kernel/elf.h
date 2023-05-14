47756b6bbb7130be3363d308e7c86e15ef786cf2
zzL1ng <haruhizz@outlook.com>
2023-05-14 17:04:45 +0800

    init lab1_c1

--- kernel/elf.h
+++ kernel/elf.h
@@ -6,6 +6,47 @@
 
 #define MAX_CMDLINE_ARGS 64
 
+/* Legal values for sh_type (section type).  */
+
+#define SHT_NULL            0        /* Section header table entry unused */
+#define SHT_PROGBITS        1        /* Program data */
+#define SHT_SYMTAB          2        /* Symbol table */
+#define SHT_STRTAB          3        /* String table */
+#define SHT_RELA            4        /* Relocation entries with addends */
+#define SHT_HASH            5        /* Symbol hash table */
+#define SHT_DYNAMIC         6        /* Dynamic linking information */
+#define SHT_NOTE            7        /* Notes */
+#define SHT_NOBITS          8        /* Program space with no data (bss) */
+#define SHT_REL             9        /* Relocation entries, no addends */
+#define SHT_SHLIB          10        /* Reserved */
+#define SHT_DYNSYM         11        /* Dynamic linker symbol table */
+#define SHT_INIT_ARRAY     14        /* Array of constructors */
+#define SHT_FINI_ARRAY     15        /* Array of destructors */
+#define SHT_PREINIT_ARRAY  16        /* Array of pre-constructors */
+#define SHT_GROUP          17        /* Section group */
+#define SHT_SYMTAB_SHNDX   18        /* Extended section indeces */
+#define    SHT_NUM         19        /* Number of defined types.  */
+#define SHT_LOOS           0x60000000    /* Start OS-specific.  */
+#define SHT_GNU_ATTRIBUTES 0x6ffffff5    /* Object attributes.  */
+#define SHT_GNU_HASH       0x6ffffff6    /* GNU-style hash table.  */
+#define SHT_GNU_LIBLIST    0x6ffffff7    /* Prelink library list */
+#define SHT_CHECKSUM       0x6ffffff8    /* Checksum for DSO content.  */
+#define SHT_LOSUNW         0x6ffffffa    /* Sun-specific low bound.  */
+#define SHT_SUNW_move      0x6ffffffa
+#define SHT_SUNW_COMDAT    0x6ffffffb
+#define SHT_SUNW_syminfo   0x6ffffffc
+#define SHT_GNU_verdef     0x6ffffffd    /* Version definition section.  */
+#define SHT_GNU_verneed    0x6ffffffe    /* Version needs section.  */
+#define SHT_GNU_versym     0x6fffffff    /* Version symbol table.  */
+#define SHT_HISUNW         0x6fffffff    /* Sun-specific high bound.  */
+#define SHT_HIOS           0x6fffffff    /* End OS-specific type */
+#define SHT_LOPROC         0x70000000    /* Start of processor-specific */
+#define SHT_HIPROC         0x7fffffff    /* End of processor-specific */
+#define SHT_LOUSER         0x80000000    /* Start of application-specific */
+#define SHT_HIUSER         0x8fffffff    /* End of application-specific */
+
+#define STT_FUNC            2              //st_info
+
 // elf header structure
 typedef struct elf_header_t {
   uint32 magic;
@@ -37,6 +78,31 @@ typedef struct elf_prog_header_t {
   uint64 align;  /* Segment alignment */
 } elf_prog_header;
 
+// Section header
+typedef struct elf_sect_header_t {
+  uint32 sh_name;		/* Section name, index in string tbl */
+  uint32 sh_type;		/* Type of section */
+  uint64 sh_flags;		/* Miscellaneous section attributes */
+  uint64 sh_addr;		/* Section virtual addr at execution */
+  uint64 sh_offset;		/* Section file offset */
+  uint64 sh_size;		/* Size of section in bytes */
+  uint32 sh_link;		/* Index of another section */
+  uint32 sh_info;		/* Additional section information */
+  uint64 sh_addralign;	/* Section alignment */
+  uint64 sh_entsize;	/* Entry size if section holds table */
+} elf_sect_header;
+
+
+typedef struct elf64_sym {
+  uint32 st_name;		/* Symbol name, index in string tbl */
+  unsigned char	st_info;	/* Type and binding attributes */
+  unsigned char	st_other;	/* No defined meaning, 0 */
+  uint16 st_shndx;		/* Associated section index */
+  uint64 st_value;		/* Value of the symbol */
+  uint64 st_size;		/* Associated symbol size */
+} elf_symbol;
+
+
 #define ELF_MAGIC 0x464C457FU  // "\x7FELF" in little endian
 #define ELF_PROG_LOAD 1
 
@@ -57,6 +123,7 @@ typedef struct elf_ctx_t {
 
 elf_status elf_init(elf_ctx *ctx, void *info);
 elf_status elf_load(elf_ctx *ctx);
+void load_func_name(elf_ctx *ctx);
 
 void load_bincode_from_host_elf(process *p);
 
--- kernel/syscall.c
+++ kernel/syscall.c
@@ -10,8 +10,13 @@
 #include "string.h"
 #include "process.h"
 #include "util/functions.h"
+#include "elf.h"
 
 #include "spike_interface/spike_utils.h"
+
+extern elf_symbol symbols[64];
+extern char sym_names[64][32];
+extern int sym_count;
 
 //
 // implement the SYS_user_print syscall
@@ -31,6 +36,32 @@ ssize_t sys_user_exit(uint64 code) {
   shutdown(code);
 }
 
+
+int func_name_printer(uint64 ret_addr) {
+  for(int i=0;i<sym_count;i++){
+    //sprint("%d %d %d\n", ret_addr, symbols[i].st_value, symbols[i].st_size);
+    if(ret_addr >= symbols[i].st_value && ret_addr < symbols[i].st_value+symbols[i].st_size){
+      sprint("%s\n",sym_names[i]);
+      if(strcmp(sym_names[i],"main")==0) return 0;
+      return 1;
+    }
+  }
+  return 1;
+}
+
+
+ssize_t sys_backtrace(uint64 depth) {
+  //sprint("this is sys_backtrace\n");
+  uint64 trace_sp = current->trapframe->regs.sp + 32;
+  uint64 trace_ra = trace_sp + 8;
+  uint64 n = 0;
+  for(; n<depth; n++) {
+    if(func_name_printer(*(uint64*)trace_ra) == 0) return n;
+    trace_ra += 16;
+  }
+  return n;
+}
+
 //
 // [a0]: the syscall number; [a1] ... [a7]: arguments to the syscalls.
 // returns the code of success, (e.g., 0 means success, fail for otherwise)
@@ -41,6 +72,8 @@ long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, l
       return sys_user_print((const char*)a1, a2);
     case SYS_user_exit:
       return sys_user_exit(a1);
+    case SYS_user_backtrace:
+      return sys_backtrace(a1);
     default:
       panic("Unknown syscall %ld \n", a0);
   }
