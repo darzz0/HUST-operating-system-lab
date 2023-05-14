5a0369fbd7b976eb41f502c8fcea826f24d1f2e0
zzL1ng <haruhizz@outlook.com>
2023-05-14 17:45:37 +0800

    init lab2_c1

--- kernel/strap.c
+++ kernel/strap.c
@@ -59,6 +59,8 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
       //panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+      if(stval < current->trapframe->regs.sp-PGSIZE)
+        panic("this address is not available!");
       map_pages(
         current->pagetable,
         ROUNDDOWN(stval,PGSIZE),
